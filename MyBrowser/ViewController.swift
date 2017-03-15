6470221db4a850bb4372de406bbe447b1e8c43ee
friendkj <friendkj@gmail.com>
2017-03-15 19:57:39 +0900

    ㅁㅁㅁ

--- MyBrowser/ViewController.swift
+++ MyBrowser/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var mainWebView: UIWebView!
+    
+    @IBAction func bookMarkAction(_ sender: Any) {
+        let bookMarkUrl : String = bookMarkSegmentedControl.titleForSegment(at: bookMarkSegmentedControl.selectedSegmentIndex)!
+        
+        let urlString : String = "https://www.\(bookMarkUrl).com"
+        mainWebView.loadRequest(URLRequest(url: URL(string: urlString)!))
+        //urlTextField.text = urlString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let urlString : String = "https://www.facebook.com"
+        mainWebView.loadRequest(URLRequest(url: URL(string: urlString)!))
 
+        //urlTextField.text = urlString
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
